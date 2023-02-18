7f20c8aeaaf7f898341976d5440cb7451c555061
JackJohnson1 <61082483+JackJohnson1@users.noreply.github.com>
2023-02-18 13:27:26 +0000

    added key listens and tick run

--- Js_game/EventListener.js
+++ Js_game/EventListener.js
@@ -1,5 +1,37 @@
-const oneTick = new Event('clockTick');
+const oneTick = new Event('oneTick');
 let up, down, left, right = false;
 document.addEventListener('keyDown', function(e){
- if(e.)
+ if(e.key === "w"){
+  up = true;
+ }
+ if(e.key === "a"){
+  left = true;
+ }
+ if(e.key === "s"){
+  down = true;
+ }
+ if(e.key === "d"){
+  right = true;
+ }
 });
+document.addEventListener("keyup", function(e){
+ if (e.key === "w"){
+     up = false;
+ }
+ if (e.key === "a"){
+     left= false;
+ }
+ if (e.key === "s"){
+     down= false;
+ }
+ if (e.key === "d"){
+     right= false;
+ }
+});
+
+function tick(){
+ document.dispatchEvent(oneTick);
+}
+function run(){
+ setInterval(tick, 16.66);
+}
