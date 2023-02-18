3adf110eb3f964115a5e5dab1a036147d4369353
JackJohnson1 <61082483+JackJohnson1@users.noreply.github.com>
2023-02-18 19:29:01 +0000

    ball

--- GameStuff/CanvasEngine.js
+++ GameStuff/CanvasEngine.js
@@ -5,7 +5,8 @@ class CanvasEngine {
         this.ctx = canvas.getContext("2d");
         this.cHeight = canvas.height;
         this.cWidth = canvas.width;
-        this.player = {"pos" : {"x": 0, "y": 0}, "vel": {"x": 0, "y": 0}};
+        this.player = {"pos" : {"x": 100, "y": 100}, "vel": {"x": 0, "y": 0}};
+        this.score = 0
 
     }
     doThisEveryFrame() {
@@ -15,9 +16,11 @@ class CanvasEngine {
         this.ctx.fillRect(0, 0, this.cWidth, this.cHeight * 2 / 3);
         this.ctx.fillStyle = "#7CFC00";
         this.ctx.fillRect(0, this.cHeight * 2 / 3, this.cWidth, this.cHeight / 3);
-        this.ctx.drawImage(document.getElementById("mg1"), this.player.pos.x, 200, 100, 300)
-
-
+        this.ctx.drawImage(document.getElementById("mg1"), this.player.pos.x, 200, 100, 300);
+        this.ctx.drawImage(document.getElementById("ball"),this.player.pos.x+80, 300, 50, 50);
+        this.ctx.drawImage(document.getElementById("hoop"), 900, 100, 50, 50);
+        this.ctx.font = "30px Arial";
+        this.ctx.fillText(this.score+"", 100, 100);
     }
 
     initialize = () => {
@@ -28,11 +31,13 @@ class CanvasEngine {
         this.ctx.fillRect(0, 0, this.cWidth, this.cHeight * 2 / 3);
         this.ctx.fillStyle = "#7CFC00";
         this.ctx.fillRect(0, this.cHeight * 2 / 3, this.cWidth, this.cHeight / 3);
-        this.ctx.drawImage(document.getElementById("mg1"), 100, 100, 100, 300)
+        this.ctx.drawImage(document.getElementById("mg1"),  this.player.pos.x,  200, 100, 300);
+        this.ctx.drawImage(document.getElementById("ball"), this.player.pos.x+80, 300, 50, 50);
+        this.ctx.drawImage(document.getElementById("hoop"), 900, 100, 50, 50);
+        this.ctx.font = "30px Arial";
+        this.ctx.fillText(this.score+"", 100, 100);
         this.startInputListeners();
         document.addEventListener("oneTick", () => {this.doThisEveryFrame()})
-
-
     }
 
 
@@ -57,16 +62,16 @@ class CanvasEngine {
 
         document.addEventListener("oneTick", function(e){
             //console.table(cursedRef.player.pos)
-            cursedRef.player.pos.x -= keyPressed.a && cursedRef.player.pos.x>=0 ? 2 : 0;
-            cursedRef.player.pos.x += keyPressed.d && cursedRef.player.pos.x<=cursedRef.cWidth ? 2 : 0;
-            cursedRef.player.pos.y -= keyPressed.w && cursedRef.player.pos.y>=0 ? 2 : 0;
-            cursedRef.player.pos.y += keyPressed.s && cursedRef.player.pos.y<=cursedRef.cHeight ? 2 : 0;
+            cursedRef.player.pos.x -= keyPressed.a && cursedRef.player.pos.x>=-25 ? 5 : 0;
+            cursedRef.player.pos.x += keyPressed.d && cursedRef.player.pos.x<=cursedRef.cWidth-125 ? 5 : 0;
+            cursedRef.player.pos.y -= keyPressed.w && cursedRef.player.pos.y>=0 ? 5 : 0;
+            cursedRef.player.pos.y += keyPressed.s && cursedRef.player.pos.y<=cursedRef.cHeight ? 5 : 0;
         })
         function tick() {
             document.dispatchEvent(oneTick);
         }
         console.log("this works");
-        setInterval(tick, 33.33);
+        setInterval(tick, 16.66);
     }
 
 }
