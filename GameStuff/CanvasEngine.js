c29f9a53af1430cbd3176bdd68fea76688a76892
supersand21 <lukashastings38@gmail.com>
2023-02-18 18:02:31 +0000

    key press events working

--- GameStuff/CanvasEngine.js
+++ GameStuff/CanvasEngine.js
@@ -5,9 +5,11 @@ class CanvasEngine {
         this.ctx = canvas.getContext("2d");
         this.cHeight = canvas.height;
         this.cWidth = canvas.width;
+        this.player = {"pos" : {"x": 0, "y": 0}, "vel": {"x": 0, "y": 0}};
+
     }
 
-    initialize() {
+    initialize = () => {
         console.log("creating world!");
 
         // Create sky and ground
@@ -15,11 +17,40 @@ class CanvasEngine {
         this.ctx.fillRect(0, 0, this.cWidth, this.cHeight*2/3);
         this.ctx.fillStyle = "#7CFC00";
         this.ctx.fillRect(0, this.cHeight*2/3, this.cWidth, this.cHeight/3);
+        this.startInputListeners();
+
     }
 
     startInputListeners() { // Start all key input event listeners
+        let cursedRef = this;
+        let keyPressed = {w: false, a: false, s: false, d: false}
 
+        const oneTick = new Event('oneTick');
+        document.addEventListener("keydown", function(e){
+            if(e.key === "w") keyPressed.w = true;
+            if(e.key === "a") keyPressed.a = true;
+            if(e.key === "s") keyPressed.s = true;
+            if(e.key === "d") keyPressed.d = true;
+        });
+        document.addEventListener("keyup", function(e){
+            if (e.key === "w") keyPressed.w = false;
+            if (e.key === "a") keyPressed.a = false;
+            if (e.key === "s") keyPressed.s = false;
+            if (e.key === "d") keyPressed.d = false;
+        });
+
+        document.addEventListener("oneTick", function(e){
+            console.table(cursedRef.player.pos)
+            cursedRef.player.pos.x -= keyPressed.a ? 1 : 0;
+            cursedRef.player.pos.x += keyPressed.d ? 1 : 0;
+            cursedRef.player.pos.y += keyPressed.s ? 1 : 0;
+            cursedRef.player.pos.y -= keyPressed.w ? 1 : 0;
+        })
+        function tick() {
+            document.dispatchEvent(oneTick);
+        }
+        console.log("this works");
+        setInterval(tick, 100);
     }
 
-    
 }
